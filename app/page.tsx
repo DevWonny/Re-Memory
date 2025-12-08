c19fab6c9ab7f7de3cad1f7e52cc5f7d6ff841bc
DevWonny <cjfdnjs1994@naver.com>
2025-12-08 14:35:12 +0900

    Global Color 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 // component
 import CameraBlender from "./components/cameraBlender";
+// style
+import "@/styles/main.scss";
+
 export default function Main() {
   return (
-    <div className="main-container w-full flex">
+    <div className="main-container w-full h-screen flex">
       <div className="camera-blending-container w-[50%] h-screen">
         <CameraBlender />
       </div>
-      <div className="folder-container">Folder Container</div>
+      {/* 폴더로 표출될 영역. 이미지가 저장 될때 카테고리(여행지) 별로 저장을 할 예정. 폴더는 해당 카테고리를 의미함. */}
+      <div className="folder-container w-[50%] h-screen">Folder Container</div>
     </div>
   );
 }
