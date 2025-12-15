25a1a9e1776d33182bd7d0b9c684829f6501d4da
DevWonny <cjfdnjs1994@naver.com>
2025-12-15 11:21:42 +0900

    Input Change Event 구현 중

--- app/components/commonInput.tsx
+++ app/components/commonInput.tsx
@@ -4,10 +4,23 @@ interface InputType {
   type: string;
 }
 export default function CommonInput({ type }: InputType) {
+  const onInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let MAX_LENGTH;
+    if (type === "category") {
+      MAX_LENGTH = 30;
+    } else if (type === "description") {
+      MAX_LENGTH = 100;
+    }
+    const value = e.target.value;
+
+    if (value.length === MAX_LENGTH) {
+      return;
+    }
+  };
   return (
     // * 카테고리 (여행지)-> 텍스트 입력(30자 내로) / 폴더 설명(100자 내로)
     <div className="common-input-container">
-      <input type="text" placeholder={type} />
+      <input type="text" placeholder={type} onChange={onInputChangeHandler} />
     </div>
   );
 }
