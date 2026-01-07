e4f8df54e002f62bcb08751898645744aa3c2ba7
DevWonny <cjfdnjs1994@naver.com>
2026-01-07 14:33:24 +0900

    Modify Page 추가

---
+++ app/modify/[id]/page.tsx
@@ -0,0 +1,13 @@
+// * 수정 페이지
+// * 전체적인 레이아웃은 Upload 페이지와 동일하게
+// * 버튼 명칭과 라우팅 위치, 해당 카테고리 이미지들을 그대로 표출해야 함
+// style
+import "@/styles/modify.scss";
+
+export default function Modify() {
+  return (
+    <div className="modify-page w-full h-full ">
+      <p>수정 페이지</p>
+    </div>
+  );
+}
